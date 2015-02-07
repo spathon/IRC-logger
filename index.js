aad5738d105262004e18b7407aa04cfd64564dd2
Patrik Spathon <patrik@spathon.com>
2015-02-08 00:57:50 +0100

    Refactor and list all messages

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ var config       = require('./config'),
     cookieParser = require('cookie-parser'),
     bodyParser   = require('body-parser'),
 
-    app = express();
+    irc_client   = require('./lib/irc'),
+    app          = express();
 
+
+
+/*=============================================*\
+  Setup Express
+\*=============================================*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,53 +28,21 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+/*=============================================*\
+  Routes
+\*=============================================*/
+
 var routes = require('./routes/index');
 app.use('/', routes);
 
 
 
-var irc = require('irc'),
-    client = new irc.Client('irc.freenode.net', config.nick, {
-      realName: config.real_name,
-      channels: config.channels,
-    }),
-    mysql = require('mysql'),
-    db  = mysql.createPool({
-      connectionLimit : 50,
-      host: config.mysql.host,
-      user: config.mysql.user,
-      password: config.mysql.password,
-      database: config.mysql.database
-    });
-
-
-console.log('Lets go');
-
-client.addListener('error', function(message) {
-    console.log('Error: ', message);
-});
-
-
-client.addListener('message', function (from, channel, text) {
-  // console.log(from + ' => ' + channel + ': ' + text);
-
-  var reg = new RegExp('/'+ config.nick +'/', 'i'),
-      mention = (text.search(reg) != -1),
-      message = {
-        from: from,
-        message: text,
-        channel: channel,
-        mention: mention,
-        pm: (channel === config.nick)
-      };
-  db.query('INSERT INTO messages SET ?', message, function(err, result) {
-    if(err) console.log('Mysql Error: ', err);
-  });
-});
-
+/*=============================================*\
+  Start the Express server
+\*=============================================*/
 
 app.set('port', process.env.PORT || 1988);
-
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 'use strict';
 var express = require('express'),
-    router = express.Router();
+    router  = express.Router(),
+    moment  = require('moment'),
+    db      = require('../lib/db');
+
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'IRC Logger' });
+router.get('/', function(req, res, next) {
+
+  db.query('SELECT * FROM messages LIMIT 200', function(err, messages) {
+    if(err) return next(err);
+
+    res.render('index', { messages: messages, moment: moment });
+  });
 });
 
 module.exports = router;
