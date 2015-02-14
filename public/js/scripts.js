237032ac4ce659cc6dd7aa49bd8bb61bf174495c
Patrik Spathon <patrik@spathon.com>
2015-02-14 08:37:11 +0100

    Add a submit new message form

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function(d) {
+
+  var add_message_form = d.getElementById('add_message_form'),
+      the_message = d.getElementById('the_message');
+
+  add_message_form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Submit the message to the server
+  })
+
+
+})(document);
