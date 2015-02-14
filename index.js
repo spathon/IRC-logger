74a6869562565ec083640191b4042b962afd6404
Patrik Spathon <patrik@spathon.com>
2015-02-14 09:06:03 +0100

    add message with sockets

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var config       = require('./config'),
     express      = require('express'),
     path         = require('path'),
@@ -6,8 +8,22 @@ var config       = require('./config'),
     cookieParser = require('cookie-parser'),
     bodyParser   = require('body-parser'),
 
+    // IRC inside sockets?
     irc_client   = require('./lib/irc'),
-    app          = express();
+    app          = express(),
+    server       = require('http').Server(app),
+    io           = require('socket.io')(server);
+
+
+
+/*=============================================*\
+  Start the Express server
+\*=============================================*/
+
+app.set('port', process.env.PORT || 1988);
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
 
 
 
@@ -39,10 +55,24 @@ app.use('/', routes);
 
 
 /*=============================================*\
+  Socket.io
+\*=============================================*/
+
+io.on('connection', function (socket) {
+  socket.on('add message', function (data) {
+    // @todo select channel if more than one and error if none
+    irc_client.say(config.channels[0], data);
+  });
+});
+
+
+
+
+/*=============================================*\
   Start the Express server
 \*=============================================*/
 
-app.set('port', process.env.PORT || 1988);
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
+// app.set('port', process.env.PORT || 1988);
+// var server = app.listen(app.get('port'), function() {
+//   console.log('Express server listening on port ' + server.address().port);
+// });
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,10 @@
 'use strict';
 
+var socket = io.connect();
+
 (function(d) {
+
+
 
   var add_message_form = d.getElementById('add_message_form'),
       the_message = d.getElementById('the_message');
@@ -8,8 +12,8 @@
   add_message_form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    // Submit the message to the server
-  })
+    socket.emit('add message', the_message.value);
+  });
 
 
 })(document);
